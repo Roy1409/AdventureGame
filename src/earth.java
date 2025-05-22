4103e8e640c0320e35cccca8cb7c4bbb4f092939
BT_1N3_05 <@>
2025-05-22 13:54:04 -0400

    a

--- src/earth.java
+++ src/earth.java
@@ -59,4 +59,94 @@ public class earth extends Character {
 
         animation = new Animation(images, 50);
         idle = new Animation(x, 50);
-    }}
+    }
+    public int getxCoord() {
+        if (facingRight) {
+            return xCoord;
+        } else {
+            return (xCoord + (getPlayerImage().getWidth()));
+        }
+    }
+
+    public int getyCoord() {
+        return yCoord;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getHeight() {
+        return getPlayerImage().getHeight();
+    }
+
+    public int getWidth() {
+        if (facingRight) {
+            return getPlayerImage().getWidth();
+        } else {
+            return getPlayerImage().getWidth() * -1;
+        }
+    }
+
+    public void faceRight() {
+        facingRight = true;
+    }
+
+    public void faceLeft() {
+        facingRight = false;
+    }
+
+    public void turn() {
+        if (facingRight) {
+            faceLeft();
+        } else {
+            faceRight();
+        }
+    }
+
+    public void moveRight() {
+        if (xCoord + MOVE_AMT <= 920) {
+            xCoord += MOVE_AMT;
+            y=false;
+        }
+    }
+
+    public void moveLeft() {
+        if (xCoord - MOVE_AMT >= 0) {
+            xCoord -= MOVE_AMT;
+            y=false;
+        }
+    }
+
+
+    public void moveUp() {
+        if (yCoord - MOVE_AMT >= 0) {
+            yCoord -= MOVE_AMT;
+            y=false;
+        }
+    }
+
+    public void moveDown() {
+        if (yCoord + MOVE_AMT <= 435) {
+            yCoord += MOVE_AMT;
+            y=false;
+        }
+    }
+
+    public BufferedImage getPlayerImage() {
+        if (!y) {
+            return animation.getActiveFrame(); }
+        return idle.getActiveFrame();
+    }
+
+    public Rectangle playerRect() {
+        int imageHeight = getPlayerImage().getHeight();
+        int imageWidth = getPlayerImage().getWidth();
+        Rectangle rect = new Rectangle(xCoord, yCoord, imageWidth, imageHeight);
+        return rect;
+    }
+
+    public void idle() {
+        y=true;
+    }
+}
