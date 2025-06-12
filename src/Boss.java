b0aa053cade26828fc8764985940d54fce987ec9
RoyCh_5 <@>
2025-06-11 22:53:02 -0400

    fixes

--- src/Boss.java
+++ src/Boss.java
@@ -72,7 +72,7 @@ public class Boss {
         animation = new Animation(images, 450);
         /*  idle = new Animation(x, 100);*/
         /* jump= new Animation(jumps,100);*/
-        attacking=new Animation(attk,66);
+        attacking=new Animation(attk,40);
     }
 
     public int getxCoord() {
